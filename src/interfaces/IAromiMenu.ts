e9bd14ffc7b6cf16e3c2b77773f868061a96433d
Elias Ojala <git@eliasojala.me>
2018-05-20 21:12:26 +0300

    Created IAromiMenu

---
+++ src/interfaces/IAromiMenu.ts
@@ -0,0 +1,37 @@
+interface IAromiMenu {
+  Start: string;
+  End: string;
+  Name: string;
+  Selected: boolean;
+
+  Days: Array<{
+    Date: string;
+    WeekDay: string;
+    DateStr: string;
+    IsVisible: string;
+
+    Meals: Array<{
+      MealType: string;
+      Name: string;
+      Price: string;
+      Info: null;
+      Translations: Array<{
+        LanguageId: number;
+        MealType: string;
+        Name: string;
+      }>
+      Markings: any[];
+      Nutritions: Array<{
+        Name: string;
+        Label: string;
+        Items: any[];
+      };
+    }>;
+  }>
+  Translations: Array<{
+    LanguageId: number;
+    Value: string;
+  }>;
+}
+
+export { IAromiMenu };
