701693f281bd2ae8c2138f87d96ae0ac9c64ed59
Elias Ojala <git@eliasojala.me>
2018-05-20 21:30:27 +0300

    Use production AROMI_JSON_DATA_URL

--- src/settings.ts
+++ src/settings.ts
@@ -7,10 +7,10 @@ const AROMI_MENU_URL_PREFIX: string = AROMI_ROOT_URL + "/AromiStorage/blob/menu/
 /**
  * Location of AromiMenusJsonData
  */
-// const AROMI_JSON_DATA_URL: string = AROMI_ROOT_URL + "/AromiStorage/blob/main/AromiMenusJsonData";
+const AROMI_JSON_DATA_URL: string = AROMI_ROOT_URL + "/AromiStorage/blob/main/AromiMenusJsonData";
 
 // debug stuff
 // tslint:disable-next-line:max-line-length
-const AROMI_JSON_DATA_URL: string = "https://rawgit.com/theel0ja/349371c1c563171f2a9c73a640196c17/raw/b9f1d971c6ffd47b4973b60f57acef549ec2640a/aromi-data.json";
+// const AROMI_JSON_DATA_URL: string = "https://rawgit.com/theel0ja/349371c1c563171f2a9c73a640196c17/raw/b9f1d971c6ffd47b4973b60f57acef549ec2640a/aromi-data.json";
 
 export { AROMI_ROOT_URL, AROMI_JSON_DATA_URL, AROMI_MENU_URL_PREFIX };
