29930717fafb75d56ec24eefc5926a7eaab11460
Elias Ojala <git@eliasojala.me>
2018-05-20 18:16:13 +0300

    Use AROMI_ROOT_URL in AROMI_MENU_URL_PREFIX

--- src/settings.ts
+++ src/settings.ts
@@ -2,6 +2,7 @@
  * Root of CGI's Aromi server.
  */
 const AROMI_ROOT_URL: string = "https://ruokalistatkoulutjapaivakodit.arkea.fi";
+const AROMI_MENU_URL_PREFIX: string = AROMI_ROOT_URL + "/AromiStorage/blob/menu/";
 
 /**
  * Location of AromiMenusJsonData
@@ -11,7 +12,5 @@ const AROMI_ROOT_URL: string = "https://ruokalistatkoulutjapaivakodit.arkea.fi";
 // debug stuff
 // tslint:disable-next-line:max-line-length
 const AROMI_JSON_DATA_URL: string = "https://rawgit.com/theel0ja/349371c1c563171f2a9c73a640196c17/raw/b9f1d971c6ffd47b4973b60f57acef549ec2640a/aromi-data.json";
-
-const AROMI_MENU_URL_PREFIX: string = "https://ruokalistatkoulutjapaivakodit.arkea.fi/AromiStorage/blob/menu/";
 
 export { AROMI_ROOT_URL, AROMI_JSON_DATA_URL, AROMI_MENU_URL_PREFIX };
